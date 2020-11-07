75627fc6bd51ebc9079395d239e3b536aa465b8f
cdahub <cdahub2020@gmail.com>
2020-11-07 11:04:11 +0800

    修改路由合约地址

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,7 +3,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x2D40C2dC6a357Bd1Ba150125ef68FEC01c3DCBA6'
+export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
 
 // a list of tokens by chain
 type ChainTokenList = {
