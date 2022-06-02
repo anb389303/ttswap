65ea495a1ddb6eeaf270ce02af247a6448a12980
anb389303 <78268075+anb389303@users.noreply.github.com>
2022-06-02 16:17:55 -0400

    Update hooks.ts

--- src/state/swap/hooks.ts
+++ src/state/swap/hooks.ts
@@ -89,9 +89,9 @@ export function tryParseAmount(value?: string, currency?: Currency): CurrencyAmo
 }
 
 const BAD_RECIPIENT_ADDRESSES: string[] = [
-  '0xE6091c43182EF1f0a909ffEBDE99fECd48C3CD55', // v2 factory
-  '0xf164fC0Ec4E93095b804a4795bBe1e041497b92a', // v2 router 01
-  '0x66A82123177dBb95F3Ea16D96C96e270C1fE2Ce7' // v2 router 02  kava 0x2D40C2dC6a357Bd1Ba150125ef68FEC01c3DCBA6
+  '0x3B0551249B704DE5B7e6775e48bb11bD938D2e92', // v2 factory
+  '0x9F1e09cfbD7E9e43f575269Bf67B80E3180C26b9', // v2 router 01
+  //'0x66A82123177dBb95F3Ea16D96C96e270C1fE2Ce7' // v2 router 02  kava 0x2D40C2dC6a357Bd1Ba150125ef68FEC01c3DCBA6
 ]
 
 /**
